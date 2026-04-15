166ba4cd79f2496e42c979f414038ead1f17f2db
CPatchane <code@patchane.com>
2026-04-15 22:37:08 +0200

    Bump typescript to v6

--- packages/eslint-config/typescript.js
+++ packages/eslint-config/typescript.js
@@ -9,14 +9,10 @@ module.exports = {
     "@typescript-eslint/no-use-before-define": ["error", { functions: false }],
     "no-shadow": "off",
     "@typescript-eslint/no-shadow": ["error"],
-    // We let prettier handle semicolons in the code
-    "@typescript-eslint/no-extra-semi": "off",
 
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "@typescript-eslint/prefer-interface": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-unused-vars": [
